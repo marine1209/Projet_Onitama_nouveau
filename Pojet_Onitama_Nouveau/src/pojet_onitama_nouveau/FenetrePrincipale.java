acd5fd25b02638a60f388f1c74f36ab85b481179
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-12-22 10:26:43 +0100

    Création de la classe FenetrePrincipale

---
+++ Pojet_Onitama_Nouveau/src/pojet_onitama_nouveau/FenetrePrincipale.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pojet_onitama_nouveau;
+
+/**
+ *
+ * @author nicol
+ */
+public class FenetrePrincipale {
+    
+}
